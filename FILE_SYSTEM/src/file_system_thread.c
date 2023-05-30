450dc108d305ed9f3f3f175545259eec38d52dab
TomasGliozzo <tgliozzo@frba.utn.edu.ar>
2023-05-30 13:18:33 +0000

    Respuestas Genéricas de Memoria y FS

--- FILE_SYSTEM/src/file_system_thread.c
+++ FILE_SYSTEM/src/file_system_thread.c
@@ -41,6 +41,11 @@ bool manejar_paquete_kernel(int socket_kernel)
       log_info(logger, "[FILE_SYSTEM]: Mensaje recibido de KERNEL: %s", mensaje);
       free(mensaje);
       break;
+    
+    case INSTRUCCION:
+      log_info(logger, "[FILE SYSTEM]: INSTRUCCION recibida de KERNEL");
+      recibir_instruccion_kernel();
+      break;
 
     case DESCONEXION:
       log_warning(logger, "[FILE_SYSTEM]: Conexión de KERNEL terminada.");
@@ -51,4 +56,92 @@ bool manejar_paquete_kernel(int socket_kernel)
       break;
     }
   }
+}
+
+void recibir_instruccion_kernel()
+{
+  Lista* lista = obtener_paquete_como_lista(socket_kernel);
+  CODIGO_INSTRUCCION cod_instruccion = *(CODIGO_INSTRUCCION*)list_get(lista,0);
+  char* nombre_archivo = (char*)list_get(lista,1);
+  int32_t direccion_fisica = 0;
+  int32_t tamanio = 0;
+  int32_t puntero_archivo = 0; //DEFINIR DE DONDE SALE ESTE DATO
+
+  switch (cod_instruccion)
+  {
+    case F_OPEN:
+      log_warning(logger,"CREAR ARCHIVO: <NOMBRE_ARCHIVO: %s>", nombre_archivo);
+
+      //ejecutar_f_open(nombre_archivo); TODO
+
+      enviar_mensaje_a_cliente("F_OPEN: OK", socket_kernel);
+      break;
+        
+    case F_CLOSE:
+      log_warning(logger,"CERRAR ARCHIVO: <NOMBRE_ARCHIVO: %s>", nombre_archivo);
+
+      //ejecutar_f_close(nombre_archivo); TODO
+
+      enviar_mensaje_a_cliente("F_CLOSE: OK", socket_kernel);
+      break;
+        
+    case F_SEEK:
+      int32_t posicion = *(int32_t*)list_get(lista,2); 
+      log_warning(logger,"ACTUALIZAR PUNTERO DE ARCHIVO: <NOMBRE_ARCHIVO: %s - POSICION: %d>",
+                            nombre_archivo,
+                            posicion
+                  );
+
+      //ejecutar_f_seek(nombre_archivo,posicion); TODO
+
+      enviar_mensaje_a_cliente("F_SEEK: OK", socket_kernel);
+      break;
+        
+    case F_READ:
+      direccion_fisica = *(int32_t*) list_get(lista, 2); 
+      tamanio = *(int32_t*)list_get(lista,3); 
+      puntero_archivo = 0; //DEFINIR DE DONDE SALE ESTE DATO
+      log_warning(logger,"LEER ARCHIVO: <NOMBRE_ARCHIVO: %s> - <PUNTERO ARCHIVO: %d> - <DIRECCION MEMORIA: %d>> - <TAMAÑO: %d>",
+                          nombre_archivo,
+                          puntero_archivo,
+                          direccion_fisica,
+                          tamanio);
+                          
+      //ejecutar_f_read(nombre_archivo,puntero_archivo,direccion_fisica,tamanio); TODO
+
+      enviar_mensaje_a_cliente("F_READ: OK", socket_kernel);
+      break;
+        
+    case F_WRITE:
+      direccion_fisica = *(int32_t*) list_get(lista, 2); 
+      tamanio = *(int32_t*)list_get(lista,3); 
+      puntero_archivo = 0; //DEFINIR DE DONDE SALE ESTE DATO
+      log_warning(logger,"ESCRIBIR ARCHIVO: <NOMBRE_ARCHIVO: %s> - <PUNTERO ARCHIVO: %d> - <DIRECCION MEMORIA: %d>> - <TAMAÑO: %d>",
+                          nombre_archivo,
+                          puntero_archivo,
+                          direccion_fisica,
+                          tamanio);
+
+      //ejecutar_f_write(nombre_archivo,puntero_archivo,direccion_fisica,tamanio); TODO
+
+      enviar_mensaje_a_cliente("F_WRITE: OK", socket_kernel);
+      break;
+        
+    case F_TRUNCATE:
+      tamanio = *(int32_t*)list_get(lista,2);
+      log_warning(logger, "TRUNCAR ARCHIVO: <NOMBRE_ARCHIVO: %s> - Tamaño: <TAMAÑO: %d>",
+                          nombre_archivo,
+                          tamanio);
+
+      //ejecutar_f_truncate(nombre_archivo,tamanio); TODO
+
+      enviar_mensaje_a_cliente("F_TRUNCATE: OK", socket_kernel);
+      break;
+
+    default:
+      log_error(logger,"FILE SYSTEM: ERROR: COD_INSTRUCCION DESCONOCIDO");
+      break;
+  }
+
+  list_destroy(lista);
 }
--- MEMORIA/src/memoria_thread.c
+++ MEMORIA/src/memoria_thread.c
@@ -13,7 +13,12 @@ void escuchar_kernel(int socket_kernel)
       log_info(logger, "[MEMORIA]: Mensaje recibido de KERNEL: %s", mensaje);
       free(mensaje);
       break;
-      
+
+    case INSTRUCCION:
+      log_info(logger, "[MEMORIA]: INSTRUCCION recibida de KERNEL");
+      recibir_instruccion_kernel();
+      break; 
+    
     case DESCONEXION:
       log_warning(logger, "[MEMORIA]: Conexión con KERNEL terminada.");
       return;
@@ -107,11 +112,60 @@ void recibir_instruccion_cpu()
       enviar_mensaje_a_cliente("OK",socket_cpu);
       log_info(logger,"MEMORIA: ENVIE EL MENSAJE <OK> A CPU COMO MOTIVO DE FIN DE MOV_OUT");
       break;
-
+    
   default:
     log_warning(logger, "[MEMORIA]: Código Instrucción desconocido.");
     break;
   }
   
+  list_destroy(lista_recepcion);
+}
+
+void recibir_instruccion_kernel()
+{
+  Lista* lista_recepcion;
+  lista_recepcion = obtener_paquete_como_lista(socket_kernel);
+  
+  CODIGO_INSTRUCCION cod_instruccion = *(CODIGO_INSTRUCCION *)list_get(lista_recepcion,0);
+  int pid = *(int*)list_get(lista_recepcion,1);
+  int id_segmento = *(int*)list_get(lista_recepcion,2);
+  int dire_base = *(int*)list_get(lista_recepcion,3);
+  int tamanio = *(int*)list_get(lista_recepcion,4);
+  
+  switch (cod_instruccion)
+  {
+    case CREATE_SEGMENT:
+      
+      log_info(logger,"CREAR SEGMENTO: <PID: %d> - Crear Segmento: <ID SEGMENTO: %d> - Base: <DIRECCION BASE: %d> - tamaño: <TAMAÑO: %d>",
+                      pid,
+                      id_segmento,
+                      dire_base,
+                      tamanio
+              );
+                   
+      //ejecutar_create_segment(...); TODO
+
+      enviar_mensaje_a_cliente("CREATE_SEGMENT: OK", socket_kernel);
+      break;
+
+    case DELETE_SEGMENT:
+      
+      log_info(logger, "ELIMINAR SEGMENTO: <PID: %d> - Eliminar Segmento: <ID SEGMENTO: %d> - Base: <DIRECCION BASE: %d> - tamaño: <TAMAÑO: %d>",
+                      pid,
+                      id_segmento,
+                      dire_base,
+                      tamanio
+              );
+
+      //ejecutar_delete_segment(...); TODO
+
+      enviar_mensaje_a_cliente("DELETE_SEGMENT: OK", socket_kernel);
+      break;
+
+    default:
+      log_warning(logger, "[MEMORIA]: Código Instrucción desconocido.");
+      break;
+  }
+
   list_destroy(lista_recepcion);
 }
